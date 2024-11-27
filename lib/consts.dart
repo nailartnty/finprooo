092b9ced91b83b4029f4be9c46d8f2a032d094f1
nailartnty <nnailartnty.06@gmail.com>
2024-11-27 10:14:56 +0700

    ini commentnya

---
+++ lib/consts.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// a primary color definition
+const primaryColor = Color(0xFFBB7B85);
+const primaryLightColor = Color(0xFFCD969F);
+
+  // a secondary color definition
+  const secondaryColor = Color(0xFF979797);
+  const textColor = Color(0xFF757575); 
+
+  // animationDuration time definition
+  const animationDuration = Duration(milliseconds: 200); // biasanya pake 200-400 kalau daveloper
+  
+
+  // default constant value for padding size
+  const defaultPadding = 20.0;
+
+// const untuk nyimpan value yg tetap dan gk mau diubah2
+// final  keyword untuk nyimpan library yg udah tetap
